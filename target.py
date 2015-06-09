a9e566cd97286b0ae7c66225094d40d830604dc1
Alexey Komissarouk <alexey@alexeymk.com>
2015-06-09 13:25:54 +0300

    adding my initial solution using inspect to see if that might be better

--- target.py
+++ target.py
@@ -2,3 +2,12 @@ from macro_module import macros, d, ct
 
 name = "bob"
 print d[ct(name, age=5)]
+
+age = 5
+name = "bob"
+
+person = {age: age, name: name}
+person = {age, name}
+person = dict(age=age, name=name)
+person = d[ct(age, name)]
+
